e6daf7f091e648ca878d635a48dd92a1f6f3ae70
mymacaccount <jakkrit.promma.bid@gmail.com>
2023-12-21 21:19:28 +0700

    test //config bucket

--- androidApp/src/main/java/com/example/photoprinterkmp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/photoprinterkmp/android/MainActivity.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.photoprinterkmp.Greeting
-//
+// //
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
