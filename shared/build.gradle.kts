48206adffcf61e8bf8ed73674e6c6cd53cfd1f99
mymacaccount <jakkrit.promma.bid@gmail.com>
2023-12-23 20:41:24 +0700

    added implementations ktor for common android ios

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -24,11 +24,22 @@ kotlin {
         }
     }
 
+    val ktorVersion = "2.3.5"
+
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+        }
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+        }
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
